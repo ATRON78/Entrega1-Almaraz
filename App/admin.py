9aff60cd845fe5cef6826e71d848c210164bf7e1
Agustín Almaraz <103610874+ATRON78@users.noreply.github.com>
2022-05-11 00:27:56 -0300

    Delete App directory

--- App/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Padre, Madre, Hermano
-# Register your models here.
-
-admin.site.register(Padre)
-admin.site.register(Madre)
-admin.site.register(Hermano)
